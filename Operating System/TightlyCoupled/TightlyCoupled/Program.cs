e188895fd200e2fddf893edfe55801411213cc21
octelham-cs <elhambakrey743@gmail.com>
2025-03-27 13:56:13 +0200

    added Tightly Coupled Problem lab4 part2

---
+++ Operating System/TightlyCoupled/TightlyCoupled/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace TightlyCoupled
+{
+    class smartWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Smart Worker Working...");
+        }
+    }
+    class lazyWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Lazy Worker Working...");
+        }
+    }
+    class Manager
+    {
+        smartWorker sW;
+        lazyWorker lW;
+
+        public Manager(smartWorker sW, lazyWorker lW)
+        {
+            this.sW = sW;
+            this.lW = lW;
+        }
+
+        public void ManageWorking()
+        {
+            sW.Work();
+            lW.Work();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            smartWorker sW=new smartWorker();
+            lazyWorker lW=new lazyWorker();
+            Manager mn=new Manager(sW, lW);
+            mn.ManageWorking();
+
+            Console.ReadKey();
+        }
+    }
+}
