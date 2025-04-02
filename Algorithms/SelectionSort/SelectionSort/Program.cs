692ae046705f0edf8e6a4b7e2dbe9476be281e91
octelham-cs <elhambakrey743@gmail.com>
2025-04-02 10:07:38 +0200

    added Selection Sort Algorithm which is used to sort numbers in this case Ascending

---
+++ Algorithms/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for(int i=0; i<n-1; i++)
+            {
+                int minIndex = i;
+
+                for(int j=i+1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (minIndex != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+
+        static void print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] Array = { 3, 90, 1, 6, 2, 5 };
+
+            Console.WriteLine("Array Before Sorted: ");
+            print(Array);
+
+            Console.WriteLine("Array After Sorted: ");
+            Sort(Array);
+            print(Array);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
